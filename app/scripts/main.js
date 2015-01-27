906c035373565c3e84d62d5b0841a56cd98ab819
Bob Holben <bholben@gmail.com>
2015-01-27 17:16:40 -0500

    Pull data in and populate templates, add navbar hovers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,26 @@
-console.log('The Iron Yard Rocks');
 
-$('.avatar').attr('src', user.avatar_url);
+// Change template engine settings to use {{ }} syntax.
+_.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
 
+// // Pull in the avatar image.
+// $('#avatar').attr('src', user.avatar_url);
+
+// Fill in topbar details.
+$('header .right').append(_.template($('#userTopbarTemplate').html())(user));
+
+// Fill in sidebar details.
+$('aside').prepend(_.template($('#userSidebarTemplate').html())({user: user, stars: stars}));
+
+// Populate the organizations list on the sidebar.
+var orgList = $('aside #organizations');
+orgs.forEach(function (org) {
+  orgList.append(_.template($('#orgTemplate').html())(org));
+});
+
+// Populate the repos list in the main page.
+var repoList = $('section.main .repo-list');
+repos.forEach(function (repo) {
+  repoList.append(_.template($('#repoTemplate').html())(repo));
+});
 
