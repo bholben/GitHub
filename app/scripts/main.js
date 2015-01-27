3515f36ec925d119775bd1b64b1508532c9c44f2
Bob Holben <bholben@gmail.com>
2015-01-27 11:59:37 -0500

    Copy GitHub data to local js files, link up avatars

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
 console.log('The Iron Yard Rocks');
+
+$('.avatar').attr('src', user.avatar_url);
+
+
+
---
+++ app/scripts/orgs.js
@@ -0,0 +1,24 @@
+var orgs = [
+{
+"login": "theironyard",
+"id": 2248433,
+"url": "https://api.github.com/orgs/theironyard",
+"repos_url": "https://api.github.com/orgs/theironyard/repos",
+"events_url": "https://api.github.com/orgs/theironyard/events",
+"members_url": "https://api.github.com/orgs/theironyard/members{/member}",
+"public_members_url": "https://api.github.com/orgs/theironyard/public_members{/member}",
+"avatar_url": "https://avatars.githubusercontent.com/u/2248433?v=3",
+"description": null
+},
+{
+"login": "plainmade",
+"id": 2983703,
+"url": "https://api.github.com/orgs/plainmade",
+"repos_url": "https://api.github.com/orgs/plainmade/repos",
+"events_url": "https://api.github.com/orgs/plainmade/events",
+"members_url": "https://api.github.com/orgs/plainmade/members{/member}",
+"public_members_url": "https://api.github.com/orgs/plainmade/public_members{/member}",
+"avatar_url": "https://avatars.githubusercontent.com/u/2983703?v=3",
+"description": null
+}
+];
