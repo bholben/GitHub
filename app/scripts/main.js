f4dc6a66344effd07a6639cd6cee923dc0c0ec8f
Bob Holben <bholben@gmail.com>
2015-01-27 23:04:48 -0500

    Add moment.js time functionality, delete fonts folder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,11 @@
 // Change template engine settings to use {{ }} syntax.
 _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
-
 // Fill in topbar details.
 $('header .right').append(_.template($('#userTopbarTemplate').html())(user));
 
 // Fill in sidebar details.
+user.created_at = moment(user.created_at).format('MMM DD, YYYY')
 var userStars = {user: user, stars: stars};
 $('aside').prepend(_.template($('#userSidebarTemplate').html())(userStars));
 
@@ -20,6 +20,7 @@ orgs.forEach(function (org) {
 var repoList = $('section.main .repo-list');
 repos = _.sortBy( repos, 'updated_at' ).reverse();
 repos.forEach(function (repo) {
+  repo.updated_at = moment(repo.updated_at).fromNow()
   if (repo.fork) {
     repoList.append(_.template($('#repoForkedTemplate').html())(repo));
   } else {
