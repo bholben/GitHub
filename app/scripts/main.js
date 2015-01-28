61e76e7b0077bb236f53ec85a6b0ad050ea606dc
Bob Holben <bholben@gmail.com>
2015-01-27 21:43:18 -0500

    Add more hovers & links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,12 @@
 _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
 
-// // Pull in the avatar image.
-// $('#avatar').attr('src', user.avatar_url);
-
 // Fill in topbar details.
 $('header .right').append(_.template($('#userTopbarTemplate').html())(user));
 
 // Fill in sidebar details.
-$('aside').prepend(_.template($('#userSidebarTemplate').html())({user: user, stars: stars}));
+var userStars = {user: user, stars: stars};
+$('aside').prepend(_.template($('#userSidebarTemplate').html())(userStars));
 
 // Populate the organizations list on the sidebar.
 var orgList = $('aside #organizations');
@@ -20,7 +18,12 @@ orgs.forEach(function (org) {
 
 // Populate the repos list in the main page.
 var repoList = $('section.main .repo-list');
+repos = _.sortBy( repos, 'updated_at' ).reverse();
 repos.forEach(function (repo) {
-  repoList.append(_.template($('#repoTemplate').html())(repo));
+  if (repo.fork) {
+    repoList.append(_.template($('#repoForkedTemplate').html())(repo));
+  } else {
+    repoList.append(_.template($('#repoTemplate').html())(repo));
+  }
 });
 
