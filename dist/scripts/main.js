f9c744f48eacece2bcb4fb8fe35fb43888899bb5
Bob Holben <bholben@gmail.com>
2015-01-27 23:05:25 -0500

    Build

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4853,15 +4853,13 @@ var orgs = [
 // Change template engine settings to use {{ }} syntax.
 _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
 
-
-// // Pull in the avatar image.
-// $('#avatar').attr('src', user.avatar_url);
-
 // Fill in topbar details.
 $('header .right').append(_.template($('#userTopbarTemplate').html())(user));
 
 // Fill in sidebar details.
-$('aside').prepend(_.template($('#userSidebarTemplate').html())({user: user, stars: stars}));
+user.created_at = moment(user.created_at).format('MMM DD, YYYY')
+var userStars = {user: user, stars: stars};
+$('aside').prepend(_.template($('#userSidebarTemplate').html())(userStars));
 
 // Populate the organizations list on the sidebar.
 var orgList = $('aside #organizations');
@@ -4871,7 +4869,13 @@ orgs.forEach(function (org) {
 
 // Populate the repos list in the main page.
 var repoList = $('section.main .repo-list');
+repos = _.sortBy( repos, 'updated_at' ).reverse();
 repos.forEach(function (repo) {
-  repoList.append(_.template($('#repoTemplate').html())(repo));
+  repo.updated_at = moment(repo.updated_at).fromNow()
+  if (repo.fork) {
+    repoList.append(_.template($('#repoForkedTemplate').html())(repo));
+  } else {
+    repoList.append(_.template($('#repoTemplate').html())(repo));
+  }
 });
 
