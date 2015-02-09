ef39106e2ff703c46fb8f796963a646bccb900e3
Bob Holben <bholben@gmail.com>
2015-02-09 15:20:52 -0500

    Replace Underscore templates with Handlebars templates.
     - bower install --save handlebars to modify bower.json
     - Add script tags to index.html for handlebars.js and templates.js
     - Create templates dir and move templates there
     - Remove template scripts from index.html
     - Edit main.js to reference the new templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,27 @@
 
-// Change template engine settings to use {{ }} syntax.
-_.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
-
 // Fill in topbar details.
-$('header .right').append(_.template($('#userTopbarTemplate').html())(user));
+$('header .right').append(Handlebars.templates.userTopbar(user));
 
 // Fill in sidebar details.
-user.created_at = moment(user.created_at).format('MMM DD, YYYY')
+user.created_at = moment(user.created_at).format('MMM DD, YYYY');
 var userStars = {user: user, stars: stars};
-$('aside').prepend(_.template($('#userSidebarTemplate').html())(userStars));
+$('aside').prepend(Handlebars.templates.userSidebar(userStars));
 
 // Populate the organizations list on the sidebar.
 var orgList = $('aside #organizations');
 orgs.forEach(function (org) {
-  orgList.append(_.template($('#orgTemplate').html())(org));
+  orgList.append(Handlebars.templates.org(org));
 });
 
 // Populate the repos list in the main page.
 var repoList = $('section.main .repo-list');
 repos = _.sortBy( repos, 'updated_at' ).reverse();
 repos.forEach(function (repo) {
-  repo.updated_at = moment(repo.updated_at).fromNow()
+  repo.updated_at = moment(repo.updated_at).fromNow();
   if (repo.fork) {
-    repoList.append(_.template($('#repoForkedTemplate').html())(repo));
+    repoList.append(Handlebars.templates.repoForked(repo));
   } else {
-    repoList.append(_.template($('#repoTemplate').html())(repo));
+    repoList.append(Handlebars.templates.repo(repo));
   }
 });
 
